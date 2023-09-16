c88a7fc4c67856fec01d9b6f1ecbd16bdbb9c54a
GuerreroEnier <guerreroenier@gmail.com>
2023-09-15 21:13:13 -0500

    template eslint-prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+				project: './tsconfig.json',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+	},
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+	return (
+		<>
+			<h1>Hola Mundo</h1>
+		</>
+	)
+}
+
+export default App
